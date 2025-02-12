901ba754fac30f6d7f59f8f851a4dc1c05816caf
fabian-ss <randommmusicd@gmail.com>
2025-02-12 01:11:20 -0600

    cambios en en nombres y cambios en las excepciones

--- cross_crypto/decrypt.py
+++ cross_crypto/decrypt.py
@@ -3,17 +3,20 @@ import base64
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES, PKCS1_OAEP
 
-def load_private_key(PRIVATE_KEY):
-    """
-    Carga la clave privada RSA.
-    """
-    private_key = RSA.import_key(PRIVATE_KEY)    
-    return private_key
+def loadPrivateKey(PRIVATE_KEY):
+    try:    
+        """
+        Carga la clave privada RSA.
+        """
+        private_key = RSA.import_key(PRIVATE_KEY)    
+        return private_key
+    except Exception as e:
+        print("Error al cargar la llave privada:", str(e))
+        raise
 
-def decrypt_hybrid_ts_py(encrypted_data_json: dict, PRIVATE_KEY: str) -> dict:
+def decryptHybrid(encrypted_data_json: dict, PRIVATE_KEY: str) -> dict:
     try:
         encrypted_key = encrypted_data_json['encryptedKey']
-        print("Encrypted Key:", encrypted_key) 
         
         import re
         if not re.match(r'^[A-Za-z0-9+/=]+$', encrypted_key):
@@ -24,7 +27,7 @@ def decrypt_hybrid_ts_py(encrypted_data_json: dict, PRIVATE_KEY: str) -> dict:
         nonce = base64.b64decode(encrypted_data_json['nonce'])
         tag = base64.b64decode(encrypted_data_json['tag'])
         
-        private_key = load_private_key(PRIVATE_KEY)
+        private_key = loadPrivateKey(PRIVATE_KEY)
         rsa_cipher = PKCS1_OAEP.new(private_key)
         aes_key = rsa_cipher.decrypt(encrypted_key)
         
@@ -34,5 +37,5 @@ def decrypt_hybrid_ts_py(encrypted_data_json: dict, PRIVATE_KEY: str) -> dict:
         return json.loads(decrypted_data.decode('utf-8'))
 
     except Exception as e:
-        print("Error en decrypt_hybrid_ts_py:", str(e))
+        print("Error en decryptHybrid:", str(e))
         raise
